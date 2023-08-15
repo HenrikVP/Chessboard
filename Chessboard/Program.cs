5c4bf392bd83d2b6d2cac84988c120b8309f7979
HenrikVP <hpo@tec.dk>
2023-08-15 10:34:06 +0200

    Set Pieces

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Drawing;
-
-namespace Chessboard
+﻿namespace Chessboard
 {
     internal class Program
     {
+        enum Piece { Pawns = 1, Rook, Knight, Bishop, Queen, King}
         static int counter = 0;
+        static int[,] chessArray;
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
             int size = 8;
+            chessArray = new int[size, size];
+            SetPieces();
             CountY(size);
-
+            Console.ReadKey();
         }
 
         static void DrawX(int sizeOfX, int y)
@@ -19,9 +22,13 @@ namespace Chessboard
             for (int x = 0; x < sizeOfX; x++)
             {
                 Console.SetCursorPosition(x*2 + 3, y+ 3);
-                if (counter++ % 2 == 0) Console.BackgroundColor = ConsoleColor.DarkGray;
-                else Console.BackgroundColor = ConsoleColor.White;
-                Console.WriteLine("  ");
+                if (counter++ % 2 == 0) Console.BackgroundColor = ConsoleColor.DarkBlue;
+                else Console.BackgroundColor = ConsoleColor.Blue;
+
+                if (chessArray[x, y] > 0) Console.ForegroundColor = ConsoleColor.Black;
+                else if (chessArray[x, y] < 0) Console.ForegroundColor = ConsoleColor.White;
+                else Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(chessArray[x, y].ToString().PadLeft(2, ' '));
             }
         }
 
@@ -32,6 +39,12 @@ namespace Chessboard
                 counter++;
                 DrawX(sizeOfY, y);
             }
+        }
+
+        static void SetPieces()
+        {
+            chessArray[0, 0] = (int)Piece.Rook;
+            chessArray[3, 7] = -(int)Piece.Queen;
         }
     }
 }
