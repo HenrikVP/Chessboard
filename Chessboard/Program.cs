c352e4bb32981154bf77555857d0477caf7078cd
HenrikVP <hpo@tec.dk>
2023-08-15 09:15:42 +0200

    Add project files.

---
+++ Chessboard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Chessboard
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int size = 8;
+            CountY(size);
+
+        }
+
+        static void DrawX(int sizeOfX, int y)
+        {
+            for (int x = 0; x < sizeOfX; x++)
+            {
+                Console.SetCursorPosition(x*2 + 3, y+ 3);
+                if (counter++ % 2 == 0) Console.BackgroundColor = ConsoleColor.DarkGray;
+                else Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("  ");
+            }
+        }
+
+        static void CountY(int sizeOfY) 
+        {
+            for (int y = 0; y < sizeOfY; y++)
+            {
+                counter++;
+                DrawX(sizeOfY, y);
+            }
+        }
+    }
+}
